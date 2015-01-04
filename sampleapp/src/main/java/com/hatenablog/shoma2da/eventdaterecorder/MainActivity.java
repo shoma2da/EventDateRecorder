cc2419d54c24ece502467836b34fa96173e00f1e
shoma2da <marcos.shoichi.matsuda@gmail.com>
2015-01-04 19:53:35 +0900

    ライブラリ内の不要なクラスを削除

--- sampleapp/src/main/java/com/hatenablog/shoma2da/eventdaterecorder/MainActivity.java
+++ sampleapp/src/main/java/com/hatenablog/shoma2da/eventdaterecorder/MainActivity.java
@@ -1,11 +1,9 @@
 package com.hatenablog.shoma2da.eventdaterecorder;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import com.hatenablog.shoma2da.eventdaterecorderlib.Sample;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -14,8 +12,6 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Sample.hello();
     }
 
 
